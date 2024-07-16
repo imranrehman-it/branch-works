d59422cf8df4a076416f70ca5195ff855195df89
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-07-16 12:52:50 -0400

    installed nodemon on server for autoreloading on save

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,17 @@
+
+import React from 'react';
+import { useState, useEffect } from 'react';
 export default function App() {
+
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    fetch('/localhost:3000/employees')
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+    
+  }, []);
+
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
