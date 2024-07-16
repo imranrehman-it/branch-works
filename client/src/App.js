3b46ebdfd04d9d6dc2af5646d386557e186f112d
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-07-16 13:24:04 -0400

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,15 @@ const App = () => {
   const [employees, setEmployees] = useState(null);
 
   useEffect(() => {
-    // Example of handling data received from useFetchEmployees
     if (data) {
       console.log(data);
-      setEmployees(data); // Assuming you have a state variable to store data
+      setEmployees(data);
     }
-  }, [data]); // Only re-run the effect if data changes
+  }, [data]);
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline test">Hello world!</h1>
 
       {loading && <p>Loading...</p>}
 
