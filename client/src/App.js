5beea62f99c4244fa89b16078214abe3e818fda9
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-07-16 13:38:34 -0400

    react-flow installed and simple demo built

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React, { useState, useEffect } from 'react';
 import useFetchEmployees from './hooks/fetchEmployees';
+import Flow from './components/Flow';
 
 const App = () => {
   const { data, loading, error } = useFetchEmployees();
@@ -14,7 +15,7 @@ const App = () => {
   }, [data]);
 
   return (
-    <div className="App">
+    <div className="App w-screen h-screen">
       <h1 className="text-3xl font-bold underline test">Hello world!</h1>
 
       {loading && <p>Loading...</p>}
@@ -22,6 +23,8 @@ const App = () => {
       {error && <p>Error: {error.message}</p>}
 
       {employees && <p>{employees['Name']}</p>}
+
+      <Flow />
 
     </div>
   );
