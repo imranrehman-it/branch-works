682b9d053a9a176d41a17a7d56ee85cd102f1b0c
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-07-17 17:38:18 -0400

    minor bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -19,10 +19,15 @@ const App = () => {
 
   return (
     <div className="App p-4">
+      <div className='flex flex-row items-center'>
+        <img className='h-10'src={googleIcon}/>
+        <h1 className='text-center text-xl font-semibold'>Google</h1>
+      </div>
+
       <Flow treeHead={treeHead} />
 
       <div className='flex flex-row w-full h-[50vh] m-4 gap-4 '>
-        <EmployeeData />
+        <EmployeeData emp />
         {/* <EmployeeData /> */}
       </div>
 
--- client/src/components/EmployeeData.js
+++ client/src/components/EmployeeData.js
@@ -15,11 +15,12 @@ const EmployeeData = () => {
 
   return (
     <div className='bg-slate-50 w-1/2 h-fit shadow-lg rounded-md p-8 border-slate border-2'>
-        <h1 className='text-3xl font-semibold'>{currentSelectedNode['Name']}</h1>
+        {currentSelectedNode && (<h1 className='text-3xl font-semibold'>{currentSelectedNode['Name']}</h1>)}
         <div className='flex flex-row mt-12 gap-4 h-full'>
             {currentSelectedNode && (<EmployeeCardContainer employee={currentSelectedNode} />)}
         </div>
     </div>
+
   )
 }
 
