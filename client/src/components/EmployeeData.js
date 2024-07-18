fb883972a8c60fcdf0344343db3635e8cbdb0aeb
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-07-18 16:49:46 -0400

    added logic to dynamically display selected employee data for ContactInfo

--- client/src/components/EmployeeData.js
+++ client/src/components/EmployeeData.js
@@ -25,7 +25,7 @@ const EmployeeData = () => {
 
   return (
     <div className='bg-slate-100 shadow-lg rounded-md p-4 border-slate w-[15%] flex flex-col gap-2' style={glassStyle}>
-        <ContactInfo />
+       {currentSelectedNode && (<ContactInfo currentSelectedNode={currentSelectedNode} />)}
         <CostInfo />
         <BusinessInfo />  
         <HiearchyInfo /> 
--- client/src/components/EmployeeDataComponenets/ContactInfo.jsx
+++ client/src/components/EmployeeDataComponenets/ContactInfo.jsx
@@ -3,43 +3,35 @@ import { MdEmail } from "react-icons/md";
 import { MdPhone } from "react-icons/md";
 import { FaMapLocationDot } from "react-icons/fa6";
 
-
-
-const ContactInfo = () => {
+const ContactInfo = ({ currentSelectedNode }) => {
   return (
-    <div id='contanct' className='w-full h-fit bg-white rounded-md shadow-sm flex flex-col gap-2 p-4 items-center'>
-
-            <div className='flex w-12 h-12 bg-blue-300 rounded-md'>
-
-            </div>
-            <div className='flex flex-col items-center'>
-                <p className='text-black text-nowrap text-left text-[0.8rem] font-semibold'>Joan Tyler</p>
-                <p className='text-black text-nowrap text-left text-[0.6rem] font-semibold'>CEO <span className='font-medium text-blue-300'>@Agentnoon</span></p> 
-            </div>
-           
-            <div className="flex flex-col w-full mt-5 gap-2">
-                <div className='flex flex-row gap-2 items-center'>
-                        <FaMapLocationDot className='bg-blue-300 p-1 rounded-md w-5 h-5 text-white' />
-                        <p className='text-black text-nowrap text-left text-[0.6rem] font-semibold'>Toronto, Canada</p>
-                    </div>
-                    <div className='flex flex-row gap-2 items-center'>
-                        <MdEmail className='bg-blue-300 p-1 rounded-md w-5 h-5 text-white font' />
-                        <p className='text-black text-nowrap text-left text-[0.6rem] font-semibold'>Imranisrehman@gmail.com</p>
-                    </div>
-                    <div className='flex flex-row gap-2 items-center'>
-                        <MdPhone className='bg-blue-300 p-1 rounded-md w-5 h-5 text-white' />
-                        <p className='text-black text-nowrap text-left text-[0.6rem] font-semibold'>+1 (647)-999-9999</p>
-                    </div>
-                
-            </div>
-
-            <div className='flex flex-wrap gap-2 mt-4 w-fit'>
-                <p className='bg-blue-100 w-fit h-fit px-2 py-1 rounded-md text-[0.6rem] font-semibold shadow-md text-nowrap'>Level: 1</p>
-                <p className='bg-red-100 w-fit h-fit px-2 py-1 rounded-md text-[0.6rem] font-semibold shadow-md text-nowrap'>Fixed Term - Local</p>
-                <p className='bg-green-100 w-fit h-fit px-2 py-1 rounded-md text-[0.6rem] font-semibold shadow-md text-nowrap'>👏 Exceeds Expectations</p>
-            </div>
-
+    <div id='contact' className='w-full h-fit bg-white rounded-md shadow-sm flex flex-col gap-2 p-4 items-center'>
+      <div className='flex w-12 h-12 bg-blue-300 rounded-md transition-all duration-300'>
+      </div>
+      <div className='flex flex-col items-center'>
+        <p className='text-black text-nowrap text-left text-[0.8rem] font-semibold'>{currentSelectedNode['Name']}</p>
+        <p className='text-black text-left text-center text-[0.6rem] font-semibold'>{currentSelectedNode['Job Title']}<span className='font-medium text-purple-600'> @Agentnoon</span></p>
+      </div>
+      <div className="flex flex-col w-full mt-5 gap-2">
+        <div className='flex flex-row gap-2 items-center'>
+          <FaMapLocationDot className='bg-blue-300 p-1 rounded-md w-5 h-5 text-white' />
+          <p className='text-black text-nowrap text-left text-[0.6rem] font-semibold'>{currentSelectedNode['Location']}</p>
         </div>
+        <div className='flex flex-row gap-2 items-center'>
+          <MdEmail className='bg-blue-300 p-1 rounded-md w-5 h-5 text-white font' />
+          <p className='text-black text-nowrap text-left text-[0.6rem] font-semibold'>{currentSelectedNode['Email']}</p>
+        </div>
+        <div className='flex flex-row gap-2 items-center'>
+          <MdPhone className='bg-blue-300 p-1 rounded-md w-5 h-5 text-white' />
+          <p className='text-black text-nowrap text-left text-[0.6rem] font-semibold'>+1 (647)-999-9999</p>
+        </div>
+      </div>
+      <div className='flex flex-wrap gap-2 mt-4 w-fit'>
+        <p className='bg-blue-100 w-fit h-fit px-2 py-1 rounded-md text-[0.6rem] font-semibold shadow-md text-nowrap'>Level: {currentSelectedNode['level']}</p>
+        <p className='bg-red-100 w-fit h-fit px-2 py-1 rounded-md text-[0.6rem] font-semibold shadow-md text-nowrap'>{currentSelectedNode['Employee Type']}</p>
+        <p className='bg-green-100 w-fit h-fit px-2 py-1 rounded-md text-[0.6rem] font-semibold shadow-md text-nowrap'>{currentSelectedNode['Performance']}</p>
+      </div>
+    </div>
   )
 }
 
